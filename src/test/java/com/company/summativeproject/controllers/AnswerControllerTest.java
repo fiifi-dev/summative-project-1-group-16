33f4221288a3c9a0be7c20baf3324c378a77e782
Dickson Afful <fiifi.dev@gmail.com>
2023-02-03 19:28:59 -0500

    feat: add answer test

--- src/test/java/com/company/summativeproject/controllers/AnswerControllerTest.java
+++ src/test/java/com/company/summativeproject/controllers/AnswerControllerTest.java
@@ -1,7 +1,44 @@
 package com.company.summativeproject.controllers;
 
-import static org.junit.Assert.*;
+import com.company.summativeproject.models.Answer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AnswerController.class)
 public class AnswerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private List<Answer> recordList;
+
+    @Test
+    public void shouldReturnAnAnswerInCollection() throws Exception {
+
+
+        String outputJson = mapper.writeValueAsString(recordList);
+
+        // ACT
+        mockMvc.perform(get("/magic"))                // Perform the GET request
+                .andDo(print())                          // Print results to console
+                .andExpect(status().isOk());              // ASSERT (status code is 200)
+    }
 
 }
