fa5051e5fd1997e38cf3bde8289fac7f9b6f2cc3
Dickson Afful <fiifi.dev@gmail.com>
2023-02-03 20:17:35 -0500

    feat: definition test

--- src/test/java/com/company/summativeproject/controllers/DefinitionControllerTest.java
+++ src/test/java/com/company/summativeproject/controllers/DefinitionControllerTest.java
@@ -1,7 +1,38 @@
 package com.company.summativeproject.controllers;
 
+import com.company.summativeproject.models.Definition;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(DefinitionController.class)
 public class DefinitionControllerTest {
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
 
+    // ObjectMapper used to convert Java objects to JSON and vice versa
+    private ObjectMapper mapper = new ObjectMapper();
+
+    // Testing GET /word
+    @Test
+    public void shouldReturnWord() throws Exception {
+        // ACT
+        mockMvc.perform(get("/word"))                // Perform the GET request
+                .andDo(print())                          // Print results to console
+                .andExpect(status().isOk());              // ASSERT (status code is 200)
+    }
 }
