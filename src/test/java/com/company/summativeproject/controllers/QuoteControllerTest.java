da1d4c7c5e2950a0043237e5216fd9fd298661e8
Robert Velasquez <rvela042@fiu.edu>
2023-02-03 19:27:28 -0500

    junit test for Quotes

--- src/test/java/com/company/summativeproject/controllers/QuoteControllerTest.java
+++ src/test/java/com/company/summativeproject/controllers/QuoteControllerTest.java
@@ -1,7 +1,21 @@
 package com.company.summativeproject.controllers;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class QuoteControllerTest {
 
+    @Test
+    public void testGetQuote() {
+        // Create an instance of QuoteController
+        QuoteController quoteController = new QuoteController();
+
+        // Call the getQuote method
+        String quote = String.valueOf(quoteController.getQuote());
+
+        // Assert that the returned quote is not null or empty
+        assertNotNull(quote);
+        assertFalse(quote.isEmpty());
+    }
 }
